e5e32bbf788050615d55e0c814c644d797792565
Michael <aaronhunter@live.com>
2018-11-14 02:34:04 -0600

    update

--- src/com/v1/miBudget/utilities/ClientTest.java
+++ src/com/v1/miBudget/utilities/ClientTest.java
@@ -1,5 +1,11 @@
 package com.v1.miBudget.utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
 import org.hibernate.Session;
 
 public class ClientTest {
@@ -7,10 +13,14 @@ public class ClientTest {
 	public static void main(String[] args) {
 		
 		try {
-			Session session = HibernateUtilities.getSessionFactory().openSession();
+			//Session session = HibernateUtilities.getSessionFactory().openSession();
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/miBudget", "root", "rootpassword");
 			String SQL = "SELECT version()";
-			String result = session.createNativeQuery(SQL).getSingleResult().toString();
+			//String result = session.createNativeQuery(SQL).getSingleResult().toString();
+			Statement stmt = conn.createStatement();
+			String result = stmt.executeQuery(SQL).toString();
 			System.out.format("MySQL version is %s\n", result);
+			System.out.println("stmt: " + stmt);
 			System.out.println("Test completed. Passed.");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
--- src/com/v1/miBudget/utilities/HibernateUtilities.java
+++ src/com/v1/miBudget/utilities/HibernateUtilities.java
@@ -1,5 +1,8 @@
 package com.v1.miBudget.utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.hibernate.HibernateException;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -7,8 +10,6 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-
-import com.v1.miBudget.entities.User;
 
 public class HibernateUtilities {
 
@@ -19,7 +20,6 @@ public class HibernateUtilities {
 	public static SessionFactory getSessionFactory() {
 		try {
 			if (sessionFactory == null) {
-				newSessionFactory = true;
 				// loads configuration and mappings
 	            Configuration configuration = new Configuration().configure("hibernate.cfg.xml")
 	            		.addAnnotatedClass(com.v1.miBudget.entities.User.class)
@@ -32,7 +32,8 @@ public class HibernateUtilities {
 	                    .applySettings(configuration.getProperties()).build();
 	             
 	            // builds a session factory from the service registry
-	            sessionFactory = configuration.buildSessionFactory(serviceRegistry); 
+	            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	            newSessionFactory = true;
 			}
 		} catch (HibernateException e) {
 			System.out.println("Error trying to create sessionFactory");
@@ -41,11 +42,12 @@ public class HibernateUtilities {
 			if (registry != null) {
 				StandardServiceRegistryBuilder.destroy(registry);
 			}
+			if (newSessionFactory) 
+				System.out.println("Returning new sessionFactory...");
+			else
+				System.out.println("sessionFactory already created.");
 		}
-		if (newSessionFactory) 
-			System.out.println("Returning new sessionFactory...");
-		else
-			System.out.println("Session factory already created.");
+		
 		return sessionFactory;
 	}
 }
